a77604df51973c5101d47793843d522bd49e5c3a
Matheus Milanez <mattmila96@gmail.com>
2023-01-16 18:36:03 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const products = await fetchProductsList('computador');
+
+const sectionArray = products.map((item) => createProductElement(item));
+
+sectionArray.map((item) => document.querySelector('.products').appendChild(item));
