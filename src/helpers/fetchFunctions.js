eea0640ed57b6e31773dcd2a52290c9c39bdf32a
Matheus Milanez <mattmila96@gmail.com>
2023-01-16 16:46:47 -0300

    requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,15 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+const fetchProductByQuery = (QUERY) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
+
+export const fetchProductsList = async (QUERY) => {
+  if (QUERY === undefined) {
+    throw new Error('Termo de busca não informado');
+  }
+  const response = await fetchProductByQuery(QUERY);
+  const data = await response.json();
+
+  const { results } = data;
+  return results;
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -20,11 +20,11 @@ describe('Teste a função fetchProductsList', () => {
 
   it('retorno da função é igual ao objeto computadorSearch', async () => {
     const expected = computadorSearch;
-    const actual = fetchProductsList('computador');
+    const actual = await fetchProductsList('computador');
     expect(actual).toEqual(expected);
   });
 
   it('ao chamar a função sem argumento, retorna um erro com a mensagem', async () => {
-    expect(await fetchProductsList()).toThrow(new Error('Termo de busca não informado'));
+    await expect(fetchProductsList()).rejects.toThrow(new Error('Termo de busca não informado'));
   });
 });
