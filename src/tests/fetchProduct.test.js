87bda75a1933632594b8a2172cf1b581cd292a3d
Matheus Milanez <mattmila96@gmail.com>
2023-01-17 17:03:11 -0300

    requisito 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,27 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProduct é uma função', () => {
+    expect(typeof fetchProductsList).toBe('function');
+  });
+
+  it('fetch é chamado ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+  });
+
+  it('retorno da função é igual ao objeto product', async () => {
+    const expected = product;
+    const actual = await fetchProduct('MLB1405519561');
+    expect(actual).toEqual(expected);
+  });
+
+  it('ao chamar a função sem argumento, retorna um erro com a mensagem', async () => {
+    await expect(fetchProductsList()).rejects.toThrow(new Error('ID não informado'));
   });
 });
